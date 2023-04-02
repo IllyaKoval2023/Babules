fed19cfd9d44de19f384d99c1a805e5a48b8a8e2
IllyaKoval2023 <illakoval38@gmail.com>
2023-04-02 21:54:56 +0300

    Added tables of categories and operations. The project structure has been created

---
+++ Babules/Controllers/ActivitiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Babules.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        public IActionResult ListOfOperations()
+        {
+            return View();
+        }
+        public IActionResult CreateAndUpdate()
+        {
+            return View();
+        }
+    }
+}
---
+++ Babules/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Babules.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult ListOfCategories()
+        {
+            return View();
+        }
+        public IActionResult CreateAndUpdate()
+        {
+            return View();
+        }
+    }
+}
